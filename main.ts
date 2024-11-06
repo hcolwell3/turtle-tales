1e074c8f660655ebee8ca5d181d535a0f4e2612d
hcolwell3 <holly.colwell@student.uts.edu.au>
2024-11-06 22:13:13 +1100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,23 @@
 function JumpingCrabs() {
-    //  function determining how the crab move up and down the y axis
+    //  function determining how the crab moves up and down the y-axis
     //  pause allows for time between jumps
     pause(100)
-    //  acceleration that drives crab up the y axis
+    //  acceleration that drives crab up the y-axis
     Crab.ay = 40
-    //  pause to allow for the crab to 'drop' back to sea floor
+    //  Pause to allow for the crab to 'drop' back to the seafloor
     pause(4000)
-    //  negative velocity allows for the sprite to travel down the y axis as if falling from a jump
+    //  Negative velocity allows for the sprite to travel down the y-axis as if falling from a jump
     Crab.vy = -50
 }
 
+//  Pause is added so only one life is lost per second
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_on_overlap(sprite2: Sprite, otherSprite: Sprite) {
     //  Define the rule that when a player sprite (turtle) comes into contact with the enemy sprites a life is lost
     scene.cameraShake(2, 500)
-    //  induce a screen shake when enemy sprite overlaps with player
-    //  create a custom sound effect when sprites collid
+    //  induce a screen shake when an enemy sprite overlaps with player
+    //  create a custom sound effect when sprites collide
     music.play(music.createSoundEffect(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    //  sine wave shape creates smooth sound
     //  frequency
     //  duration 600 milliseconds
     //  volume
@@ -24,18 +26,27 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_on_overlap(sp
     //  No additional effect applied
     //  Linear means it changes at a constant rate, without acceleration or deceleration
     //  play sound until complete once
-    //  loss of one life when sprites overlap
     info.changeLifeBy(-1)
-    //  Pause is added so only one life is lost per second
+    //  loss of one life when sprites overlap
     pause(1000)
 })
+// framerate 0.2seconds
 controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    //  looping animation for turtle swimming
+    //  looping animation for turtle swimming in left direction
     animation.runImageAnimation(Turtle, assets.animation`
             Turtle animationL
         `, 200, true)
 })
-//  Used functions to create levels to be able to call differerent features
+// when left button pressed turtle changes direction
+// framerate 0.2seconds
+controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
+    //  looping animation for turtle swimming in right direction
+    animation.runImageAnimation(Turtle, assets.animation`
+            Turtle animation R
+        `, 200, true)
+})
+// when right button pressed turtle changes direction
+//  Used functions to create level to be able to call differerent features
 function Level1() {
     //  Set the sprite on a starting position on the left of the screen
     Turtle.setPosition(20, 79)
@@ -70,32 +81,25 @@ function SharkEnemy() {
     Shark.setPosition(600, 20)
     //  acceleration -5 to travel toward turtle sprite start position
     Shark.ax = -15
-    //  animate shark sprite to mimic swimming through water, changing frames every 0.2 secs and continous repeat
+    //  Animate shark sprite to mimic swimming through water, changing frames every 0.2 seconds and continuous repeat
     animation.runImageAnimation(Shark, assets.animation`
         sharky
     `, 200, true)
 }
 
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    //  looping animation for turtle swimming in right direction, framerate 0.2sec
-    animation.runImageAnimation(Turtle, assets.animation`
-            Turtle animation R
-        `, 200, true)
-})
 //  define function to end level when sprite reaches 'bubbles' at the end of tile map
 scene.onOverlapTile(SpriteKind.Player, assets.tile`
-        bubble b
+        bubble b #bubble b is asset name of bubble wall
     `, function on_overlap_tile(sprite: Sprite, location: tiles.Location) {
-    //  play custom end level music, four notes at tempo 400bmp
     music.play(music.stringPlayable("B A C5 C5 - - - - ", 400), music.PlaybackMode.UntilDone)
-    //  display level one end screen
+    //  Display level one end screen
     game.splash("Level 1 complete!")
     //  wait two seconds
     pause(2000)
     //  call level two
     level2()
 })
-//  creating the function for the crabs to generate on the tilemap
+//  Creating the function for the crabs to generate on the tilemap
 function MovingCrabs() {
     
     //  Set the starting positions for all crab enemy sprites on tiles established in tilemap
@@ -133,11 +137,11 @@ function MovingCrabs() {
 let Shark : Sprite = null
 let Crab : Sprite = null
 let Turtle : Sprite = null
-//  setting the bakground image to the ocean scene designed
+//  setting the background image to the ocean scene designed
 scene.setBackgroundImage(assets.image`
     background
 `)
-//  Turtle sprite set as character
+//  Turtle sprite set as a character
 Turtle = sprites.create(assets.image`
     Turtle
 `, SpriteKind.Player)
